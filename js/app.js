7aabceeb3ae37c5e1dae1b0cc5492395bbf48752
daisy1754 <d-part@hotmail.co.jp>
2013-08-31 11:33:12 +0900

    add time textzone and handle start/stop

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 var STATE_INIT = 0, STATE_RUNNING = 1, STATE_STOPPED = 2;
 var currentState;
+var timeText;
 var startButton, stopButton, restartButton, resetButton;
+var stopwatch = new StopWatch();
 
 window.onload = function() {
   var stopwatch = new StopWatch();
@@ -8,18 +10,28 @@ window.onload = function() {
   restartButton = getById("restart_button");
   resetButton = getById("reset_button");
   stopButton = getById("stop_button");
+  timeText = getById("time");
   addEvent(startButton, 'click', handleStart);
   addEvent(stopButton, 'click', handleStop);
   addEvent(restartButton, 'click', handleRestart);
   addEvent(resetButton, 'click', handleReset);
 };
 
+function updateTimeText(millis) {
+  var sec = parseInt(millis / 1000);
+  var handred_millis = parseInt(millis % 1000 / 100);
+  var ten_millis = parseInt(millis % 100 / 10);
+  timeText.innerHTML = sec + "." + handred_millis + "" + ten_millis;
+}
+
 function handleStart() {
   setUIState(STATE_RUNNING);
+  stopwatch.start({onTimeChange: updateTimeText});
 }
 
 function handleStop() {
   setUIState(STATE_STOPPED);
+  updateTimeText(stopwatch.stop());
 }
 
 function handleRestart() {
